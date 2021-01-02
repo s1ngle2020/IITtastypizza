380f15b45a0fa86434588bd0ba0fe54c5ead9af4
s1ngle <les7tat@mail.ru>
2021-01-02 15:51:28 +0300

    Updates

---
+++ precache-manifest.0a6a36435c33099dab2324f6b6a5d788.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce867237bb3eff6d4509e60d7fd5be00",
+    "url": "/IITtastypizza/index.html"
+  },
+  {
+    "revision": "ce33046ee7359af66ab4",
+    "url": "/IITtastypizza/static/css/main.85aad9f7.chunk.css"
+  },
+  {
+    "revision": "e7b044927fb06fc34e5b",
+    "url": "/IITtastypizza/static/js/2.e39d5b70.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/IITtastypizza/static/js/2.e39d5b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce33046ee7359af66ab4",
+    "url": "/IITtastypizza/static/js/main.be7a58ab.chunk.js"
+  },
+  {
+    "revision": "3951a1d990d76f13f9f4",
+    "url": "/IITtastypizza/static/js/runtime-main.bef7182f.js"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/IITtastypizza/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "c729a92dbd5510059da984ae84edbea5",
+    "url": "/IITtastypizza/static/media/background2.c729a92d.jpg"
+  },
+  {
+    "revision": "bd72426a85c893dd886b615bef3913f0",
+    "url": "/IITtastypizza/static/media/bavarianMochentochen.bd72426a.jpg"
+  },
+  {
+    "revision": "dad1a1aab7f054dd79a927dae8806925",
+    "url": "/IITtastypizza/static/media/beefTeistiMohentochen.dad1a1aa.jpg"
+  },
+  {
+    "revision": "088926e8b9632fd5e6a9d996a70eddcc",
+    "url": "/IITtastypizza/static/media/burnAppleKiwi.088926e8.jpg"
+  },
+  {
+    "revision": "211154c4a608fcb390aa929945966640",
+    "url": "/IITtastypizza/static/media/cheeseburgerPizza.211154c4.jpg"
+  },
+  {
+    "revision": "df0a98af4d07e254ea43e39b60db02fa",
+    "url": "/IITtastypizza/static/media/chickenWings.df0a98af.jpg"
+  },
+  {
+    "revision": "d5ed15777719461da303ac8d9e032d3f",
+    "url": "/IITtastypizza/static/media/countryFourSeasons.d5ed1577.jpg"
+  },
+  {
+    "revision": "e7bd8c188fdec3ee5cc8d6b4fcda52dd",
+    "url": "/IITtastypizza/static/media/deliciousMohentochen.e7bd8c18.jpg"
+  },
+  {
+    "revision": "329c12b7653ebd9089dac9373217aee7",
+    "url": "/IITtastypizza/static/media/dorblueChicken.329c12b7.jpg"
+  },
+  {
+    "revision": "e88fb0e1b337ea641809194e75fdb544",
+    "url": "/IITtastypizza/static/media/fourCheeses.e88fb0e1.jpg"
+  },
+  {
+    "revision": "0f9397077981e2e33d49d77cc7fca296",
+    "url": "/IITtastypizza/static/media/fourSeasons.0f939707.jpg"
+  },
+  {
+    "revision": "5cfa7b74633e9626e5d7f3210b5d85b2",
+    "url": "/IITtastypizza/static/media/funLogo.5cfa7b74.jpg"
+  },
+  {
+    "revision": "14c88c1cd1358795132ad8ec951217ee",
+    "url": "/IITtastypizza/static/media/headerMainPhotos.14c88c1c.png"
+  },
+  {
+    "revision": "0479b321574f202f8c564cad0a887c33",
+    "url": "/IITtastypizza/static/media/ico-cart.0479b321.svg"
+  },
+  {
+    "revision": "844201371d6e9e81945f9c7fbc70c26d",
+    "url": "/IITtastypizza/static/media/italian.84420137.jpg"
+  },
+  {
+    "revision": "9ae6465d8d6f7acb339d8f2f4bf8b3f0",
+    "url": "/IITtastypizza/static/media/logo.9ae6465d.png"
+  },
+  {
+    "revision": "91badc5302bdbb4a56383b4ba779b3bf",
+    "url": "/IITtastypizza/static/media/logopizza.91badc53.png"
+  },
+  {
+    "revision": "15f814d4071f6d1935c4a2d53df048be",
+    "url": "/IITtastypizza/static/media/malina-webfont.15f814d4.eot"
+  },
+  {
+    "revision": "30a328fb8d06891d20b93ea13edd1b8e",
+    "url": "/IITtastypizza/static/media/malina-webfont.30a328fb.woff"
+  },
+  {
+    "revision": "635b288bb0e9b5343771e754b971cc5c",
+    "url": "/IITtastypizza/static/media/malina-webfont.635b288b.svg"
+  },
+  {
+    "revision": "6d35e3021d3609c83966f2e9c4d8b024",
+    "url": "/IITtastypizza/static/media/meat.6d35e302.jpg"
+  },
+  {
+    "revision": "6f5b9bb1dca21ce711d4cf56202ee9bd",
+    "url": "/IITtastypizza/static/media/miniPancakes.6f5b9bb1.jpg"
+  },
+  {
+    "revision": "e84e43b45bb4d3c4556f192ed30f3037",
+    "url": "/IITtastypizza/static/media/mushroomChicken.e84e43b4.jpg"
+  },
+  {
+    "revision": "0fd6dba811f8c0282e2827868536237a",
+    "url": "/IITtastypizza/static/media/mushroomMohantochen.0fd6dba8.jpg"
+  },
+  {
+    "revision": "a7cb5ab505c44e4cc82053d342651162",
+    "url": "/IITtastypizza/static/media/pepperoni.a7cb5ab5.jpg"
+  },
+  {
+    "revision": "15ef841afb9f8f33b8a5114bd4517ee3",
+    "url": "/IITtastypizza/static/media/philadelphia.15ef841a.jpg"
+  },
+  {
+    "revision": "fad581a35cde748533603e66a94be08a",
+    "url": "/IITtastypizza/static/media/pizzaFox.fad581a3.jpg"
+  },
+  {
+    "revision": "786fa9ec874a7a1a6d9108df0c7e76db",
+    "url": "/IITtastypizza/static/media/pizzaname.786fa9ec.png"
+  },
+  {
+    "revision": "93354e6a1fa6b1e3396e70336ed17ba4",
+    "url": "/IITtastypizza/static/media/porkTastySauce.93354e6a.jpg"
+  },
+  {
+    "revision": "00a8d77415484e0530ba0816ea475445",
+    "url": "/IITtastypizza/static/media/spicyChili.00a8d774.jpg"
+  },
+  {
+    "revision": "7e75d0a15d991ac01991b44cec68f1f3",
+    "url": "/IITtastypizza/static/media/ukrainian.7e75d0a1.jpg"
+  },
+  {
+    "revision": "99a45c489beb584916e9c49bbea5dacc",
+    "url": "/IITtastypizza/static/media/vegetarian.99a45c48.jpg"
+  }
+]);
